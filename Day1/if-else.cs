d7b07ee74cfb88b60d2f63a5dbb3f3100e21f9a7
Yoonho Jeong <109999749+edaild@users.noreply.github.com>
2024-01-28 15:47:08 +0900

    Add files via upload

---
+++ Day1/if-else.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+class MianClass { 
+    
+    public static void Main(String[] args){
+
+        int num = 10;
+
+        if (num > 0)
+            Console.WriteLine("양수");
+        else if (num < 0)
+            Console.WriteLine("음수");
+        else
+            Console.WriteLine("영");
+    }
+}
---
+++ Day1/while-for.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Mainclass{
+    public static void Main(string[] args)
+    {
+        // 1 부터 10까지 프로그래밍을 작성하시오.
+        // 반복문을 안사용하면
+        Console.WriteLine("1");
+        Console.WriteLine("2");
+        Console.WriteLine("3");
+        Console.WriteLine("4");
+        Console.WriteLine("4");
+        Console.WriteLine("5");
+        Console.WriteLine("6");
+        Console.WriteLine("7");
+        Console.WriteLine("9");
+        Console.WriteLine("10");
+
+      
+     // for 문 사용시
+     for( int i = 1; i<11;i++) {
+         Console.WriteLine(i);
+     }
+       
+
+        // while 문 상용시
+        int i = 1;
+            while (i < 11)
+            Console.WriteLine(i++);
+            
+    }
+
+} 
+
+
